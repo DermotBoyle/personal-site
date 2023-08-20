9b60f7c748c0450bab6811920df4a148166e0c61
dermot <dermot@weaver.build>
2023-08-20 15:40:47 +0200

    fix respsonsive navbar

--- app/common/components/navbar/index.tsx
+++ app/common/components/navbar/index.tsx
@@ -3,7 +3,7 @@ import { NAV_PAGES } from "../../constants";
 
 export const Navbar = () => {
     return (
-        <nav className='w-[75vw] mx-auto pt-4 hidden md:block shadow-inner bg-black items-center font-mono text-lg lg:flex justify-end'>
+        <nav className='w-[75vw] mx-auto pt-4 hidden md:flex shadow-inner bg-black items-center font-mono text-lg justify-end'>
             {NAV_PAGES.map((page, i) => <Link className="text-whuite ml-12 first:ml-0" key={i} href={page.path}><p>{page.name}</p></Link>)}
         </nav>
     );

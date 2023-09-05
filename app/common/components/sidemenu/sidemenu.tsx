96f5630a5c034fb134d6b6b197e89b4d42440523
Dermot Boyle <dermotboyled@gmail.com>
2023-09-05 14:42:52 +0200

    sidemenu fix

--- app/common/components/sidemenu/sidemenu.tsx
+++ app/common/components/sidemenu/sidemenu.tsx
@@ -7,7 +7,7 @@ const SideMenu = () => {
 
 	const { setIsOpen, isOpen } = useContext(SideMenuContext)
 
-	return <div className={`z-10 py-12 flex-col justify-around text-center absolute top-0 right-0 w-[75vw] h-screen bg-black ease-in-out duration-300 ${isOpen ? "translate-x-0 " : "translate-x-full"}`}>
+	return <div className={`z-10 py-12 flex-col justify-around text-center w-[75vw] fixed top-0 right-0 h-screen bg-black ease-in-out duration-300 transform ${isOpen ? "translate-x-0" : "translate-x-full"}`}>
 		{NAV_PAGES.map((page, i) => {
 			return <Link href={page.path} onClick={() => setIsOpen(false)} key={i} className="block text-white h-16 first:mt-8 hover:cursor-pointer">{page.name}</Link>
 		})}

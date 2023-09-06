e5ecf4fb7dde51944df4ce88f3ac56140d2d5ad1
Dermot Boyle <dermotboyled@gmail.com>
2023-09-06 16:20:11 +0200

    added new image

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -20,26 +20,27 @@ const AboutPage = () => {
 
       <section className='md:w-[45vw] w-[80vw] mt-12 mb-24'>
         <h3 className='text-2xl font-medium'>Work history</h3>
+
         <ul>
-          <li className='grid grid-flow-col items-center pt-12 gap-8'>
-            <div>
+          <li className='flex pt-4'>
+            <div className='mr-6 w-[15%] flex flex-col items-start'>
               <p className='text-sm'>2022 - Now</p>
-              <p className='text-xs m-auto text-orange-accent font-medium text-center'>(Contract)</p>
+              <p className='text-xs text-orange-accent font-medium text-center'>(Contract)</p>
             </div>
-            <div className='col-span-2 align-start'>
+            <div className='w-[70%]'>
               <a className='hover:underline underline-offset-2 decoration-2 font-medium' href='https://weaver.build' rel='noopener' target='_blank'>Senior Fullstack engineer <span className='text-orange-accent'>@</span> Weaver</a>
               <p className='text-secondary text-sm'>Remote</p>
               <p className='text-sm'>Technical lead of feature development for mobile and web products in React, NextJS, AWS, Stripe</p>
-              <p className='text-sm'>Unit and E2E testing - Storybook, playwright, Jest</p>
+              <p className='text-sm'>Unit and E2E testing - Storybook, Playwright, Jest</p>
             </div>
           </li>
 
-          <li className='grid grid-flow-col gap-8 items-center pt-12'>
-            <div>
+          <li className='flex pt-4'>
+            <div className='mr-6 w-[15%] flex flex-col items-start'>
               <p className='text-sm'>2021 - 2022</p>
-              <p className='text-xs m-auto text-orange-accent font-medium text-center'>(Contract)</p>
+              <p className='text-xs text-orange-accent font-medium text-center'>(Contract)</p>
             </div>
-            <div className='col-span-2'>
+            <div className='col-span-11'>
               <a className='hover:underline underline-offset-2 decoration-2 font-medium' href='https://weaver.build' rel='noopener' target='_blank'>Product engineer <span className='text-orange-accent'>@</span> Spark</a>
               <p className='text-secondary text-sm'>Remote</p>
               <p className='text-sm'>Technical lead on MVP client projects</p>
@@ -48,15 +49,46 @@ const AboutPage = () => {
             </div>
           </li>
 
-          <li className='grid grid-flow-col gap-8 items-center pt-12'>
-            <p className='text-sm'>2020 - 2021</p>
+          <li className='flex pt-4'>
+            <div className='mr-6 w-[15%] flex flex-col items-start'>
+              <p className='text-sm'>2020 - 2021</p>
+              <p className='text-xs text-orange-accent font-medium text-center'>(Perm)</p>
+            </div>
             <div className='col-span-2'>
               <a className='hover:underline underline-offset-2 decoration-2 font-medium' href='https://adidas.com' rel='noopener' target='_blank'>Frontend engineer <span className='text-orange-accent'>@</span> adidas</a>
               <p className='text-secondary text-sm'>Zaragoza, Espana</p>
-              <p className='text-sm'>Engineer for Brand and Creation team</p>
+              <p className='text-sm'>Engineer for Brand and Creation division</p>
               <p className='text-sm'>Member of Origo project - custom tooling for shoe designers</p>
               <p className='text-sm'>Member of YARN - Design system development across adidas digital products</p>
               <p className='text-sm'>Unit and Integration testing - Storybook, Jest, React testing library</p>
+            </div>
+          </li>
+
+          <li className='flex pt-4'>
+            <div className='mr-6 w-[15%] flex flex-col items-start'>
+              <p className='text-sm'>2020 - 2020</p>
+              <p className='text-xs text-orange-accent font-medium text-center'>(Perm)</p>
+            </div>
+            <div className='col-span-2'>
+              <a className='hover:underline underline-offset-2 decoration-2 font-medium' href='https://fundacionucjc.org/en/teacher-training/' rel='noopener' target='_blank'>Technical lead <span className='text-orange-accent'>@</span> Ximdex</a>
+              <p className='text-secondary text-sm'>Remote</p>
+              <p className='text-sm'>Lead developer for project Each Teach funded by the University Camilo Jose Cela</p>
+              <p className='text-sm'>Hybrid mobile and Web app built with React & NextJS</p>
+              <p className='text-sm'>Design system development with CSS-in-JS</p>
+              <p className='text-sm'>Unit testing - Jest</p>
+            </div>
+          </li>
+
+          <li className='flex pt-4'>
+            <div className='mr-6 w-[15%] flex flex-col items-start'>
+              <p className='text-sm'>2019 - 2020</p>
+              <p className='text-xs text-orange-accent font-medium text-center'>(Perm)</p>
+            </div>
+            <div className='col-span-2'>
+              <a className='hover:underline underline-offset-2 decoration-2 font-medium' href='https://www.linkedin.com/company/arbor-fintech/' rel='noopener' target='_blank'>Frontend developer <span className='text-orange-accent'>@</span> Arbor (defunct)</a>
+              <p className='text-secondary text-sm'>Madrid, Espana</p>
+              <p className='text-sm'>Fintech start-up</p>
+              <p className='text-sm'>Hybrid mobile and Web app built with Angular</p>
             </div>
           </li>
         </ul>
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,16 @@
 'use client'
 import dynamic from "next/dynamic"
 
-
 const CalendlyPopUp = dynamic(() => import("./common/components/Calendly"), { ssr: false, }); // No ssr
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-24">
-      <section className="md:w-[45vw] w-[80vw] mt-24 mb-24">
-        <p className="text-xl text-secondary">
+      <section className="md:w-[45vw] w-[80vw] mt-24 mb-24 relative">
+        <p className="text-xl text-secondary z-1">
           Hi, my name is
         </p>
-        <h3 className="text-4xl mt-4 font-semibold">
+        <h3 className="text-4xl mt-4 font-semibold z-1">
           Dermot Boyle.
         </h3>
         <h3 className="text-4xl mt-4 text-secondary">
@@ -20,8 +19,10 @@ export default function Home() {
         <p className="mt-6 text-secondary">For me, it's all about crafting top-notch products that fit perfectly for <b className="text-white">start-ups,</b> <b className="text-white">small businesses,</b> and <b className="text-white">enterprises.</b> I bring a ton of value as a team player, making sure every project is a real game-changer.
         </p>
         <p className="mt-4 text-secondary">
-        Currently, I'm working on an exciting hybrid mobile and web application project at <b className='text-orange-accent underline underline-offset-4 hover:cursor-pointer'><a rel="noreferrer noopener" target="_blank" href="https://www.weaver.build/">Weaver.</a></b>
+          Currently, I'm working on an exciting hybrid mobile and web application project at <b className='text-orange-accent underline underline-offset-4 hover:cursor-pointer'><a rel="noreferrer noopener" target="_blank" href="https://www.weaver.build/">Weaver.</a></b>
         </p>
+        <div className="hidden md:block shadow-2xl shadow-black absolute opacity-30 rounded-full top-[-2vh] right-[-20vw] w-[40vw] h-[40vw] bg-center bg-[url('/images/Dermot_Logo.png')] z-[-1]">
+        </div>
       </section>
       <CalendlyPopUp />
     </main>

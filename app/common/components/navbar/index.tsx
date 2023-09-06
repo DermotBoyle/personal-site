fa0eabcd8e96445e2d14c1f1ab9bb0ecf0dfc2b0
Dermot Boyle <dermotboyled@gmail.com>
2023-09-06 08:30:27 +0200

    added underline animation for nav

--- app/common/components/navbar/index.tsx
+++ app/common/components/navbar/index.tsx
@@ -1,10 +1,15 @@
+'use client';
 import Link from "next/link";
 import { NAV_PAGES } from "../../constants";
+import { usePathname } from 'next/navigation'
 
 export const Navbar = () => {
+
+    const currentPath = usePathname()
+
     return (
         <nav className='w-[75vw] mx-auto pt-4 hidden md:flex shadow-inner bg-black items-center font-mono text-lg justify-end'>
-            {NAV_PAGES.map((page, i) => <Link className="text-whuite ml-12 first:ml-0" key={i} href={page.path}><p>{page.name}</p></Link>)}
+            {NAV_PAGES.map((page, i) => <Link className={`text-white ml-12 first:ml-0 ${page.path === currentPath ? 'underline underline-offset-8 decoration-2' : undefined} relative ${page.path !== currentPath ? 'after:bg-white after:absolute after:h-[2px] after:w-0 after:bottom-0 after:top-7 after:left-0 hover:after:w-full after:transition-all after:duration-300' : undefined}  cursor-pointer`} key={i} href={page.path}><p>{page.name}</p></Link>)}
         </nav>
     );
 };
--- app/hooks/animation/useFadeInButtonBorder.tsx
+++ app/hooks/animation/useFadeInButtonBorder.tsx
@@ -36,7 +36,6 @@ export const useFadeInButtonBorder = () => {
         } else {
           entry.target.classList.add('animate-borderFadeInHeight')
         }
-
       }
     })
   }

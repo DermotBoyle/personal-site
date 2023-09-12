8ff55af677c173f24f6259178da7b273307a59be
Dermot Boyle <dermotboyled@gmail.com>
2023-09-12 19:13:40 +0200

    added uri for jobs site

--- app/dataLayer/projectBrief.ts
+++ app/dataLayer/projectBrief.ts
@@ -1,26 +1,26 @@
 const getProjectBriefs = () =>
-[
+	[
 		{
 			title: 'Travel Madrid',
 			information: 'Madrid transport iOS mobile app',
-			techUsed: ['Swift', 'SwiftUI', 'Xcode', 'CocoaPods'],
+			techUsed: [ 'Swift', 'SwiftUI', 'Xcode', 'CocoaPods' ],
 			link: '/projects/travel-madrid'
 		},
 		{
-			title: 'RemoteNorway.com',
+			title: 'norwayremotejobs.com',
 			information: 'A jobs board for Norwegian tech opportunities',
-			techUsed: ['Typescript', 'NextJS', 'AWS'],
+			techUsed: [ 'Typescript', 'NextJS', 'AWS' ],
 		},
 		{
 			title: 'Bookster.ai',
 			information: 'A hotel booking mobile app created with React Native',
-			techUsed: ['React Native', 'Expo', 'TypeScript', 'SCSS'],
+			techUsed: [ 'React Native', 'Expo', 'TypeScript', 'SCSS' ],
 		},
 		{
 			title: 'Goya Twitter bot',
 			information: 'A Twitter bot that tweets Goya sketches with explanations',
-			techUsed: ['Node.js',  'AWS Lambda', 'AWS CloudWatch', 'TypeScript', 'Twitter API'],
+			techUsed: [ 'Node.js', 'AWS Lambda', 'AWS CloudWatch', 'TypeScript', 'Twitter API' ],
 		}
 	];
 
-	export default getProjectBriefs;
+export default getProjectBriefs;
